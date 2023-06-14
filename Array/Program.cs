511771f7953299ce27180f85ab99897ec868c003
Eze <Eze@DESKTOP-PGAGFI4>
2023-06-14 15:39:37 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,43 @@
+﻿//Strings
+/*
+string institute = "AfriHub ICT Institue";
+string city = "Abuja";
+Console.WriteLine(institute);
+
+Console.WriteLine(institute.IndexOf ('I')); //to find the index of a letter 
+Console.WriteLine (institute  + " is located in " + city);
+
+
+
+string studentAge1 = "13", studentAge2 = "70", studentAge3 = "89";
+Console.WriteLine(studentAge1 + studentAge2);
+
+Console.WriteLine(Convert.ToInt32(studentAge1) + Convert.ToInt32(studentAge3));
+
+
+//NUMBERS
+int a = 120, b = 60, c=10,d=10;
+Console.WriteLine(a / d-b*c+10);
+
+Console.WriteLine(Math.Max(4, 25));
+Console.WriteLine(Math.Abs(-564));
+
+
+// as a programer no more bodmas use pemdas parentesis exponet multiplication division aition and subtraction.
+*/
+
+//To collect user input  
+Console.WriteLine("what is your Name:");
+string myName = Console.ReadLine();
+
+Console.WriteLine("how old are you ?");
+string myAge = Console.ReadLine();
+
+Console.WriteLine("wetin dey happen?");
+string deyHappen =Console.ReadLine();
+
+Console.WriteLine("my name is " + myName + ", I am " + myAge + " years Old, , and i dey o " + deyHappen );
+
+Console.ReadLine();
+
+
